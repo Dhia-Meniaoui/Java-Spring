31d1ad362d791495870866c824ebdf5765a8eb7a
dhia <diaminiaoui@gmail.com>
2022-09-13 23:00:47 +0100

    second commit

--- Microservices/movie-catalog-service/movie-catalog-service/src/main/java/com/Dhia/moviecatalogservice/controller/MovieCatalogController.java
+++ Microservices/movie-catalog-service/movie-catalog-service/src/main/java/com/Dhia/moviecatalogservice/controller/MovieCatalogController.java
@@ -4,11 +4,16 @@ package com.Dhia.moviecatalogservice.controller;
 import com.Dhia.moviecatalogservice.models.Catalog;
 import com.Dhia.moviecatalogservice.models.Movie;
 import com.Dhia.moviecatalogservice.models.Rating;
+import com.Dhia.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -19,19 +24,18 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
-
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
     @RequestMapping("/{userId}")
     public List<Catalog> getCatalog(@PathVariable("userId") String userId){
 
-        RestTemplate restTemplate = new RestTemplate();
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("gwer","1"),
-                new Rating("ager","2"),
-                new Rating("dsga","3")
-        );
-        return ratings.stream().map(rating -> {
-                Movie movie = restTemplate.getForObject("http://localhost:8082/movies/dhia", Movie.class);
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/rating/users/dhia", UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating -> {
+               Movie movie = restTemplate.getForObject("http://localhost:8082/movies/dhia", Movie.class);
                 return new Catalog(movie.getName(),"test", 4);
                 })
                 .collect(Collectors.toList());
