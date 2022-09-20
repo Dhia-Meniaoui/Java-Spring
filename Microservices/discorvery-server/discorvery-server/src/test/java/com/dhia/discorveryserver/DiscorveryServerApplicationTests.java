1a216e8dfd60575acedd93918ace4ddf33aa7e55
dhia <diaminiaoui@gmail.com>
2022-09-20 19:28:15 +0100

    fourth commit

---
+++ Microservices/discorvery-server/discorvery-server/src/test/java/com/dhia/discorveryserver/DiscorveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhia.discorveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscorveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
